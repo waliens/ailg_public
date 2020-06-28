dc5ef4eefbb0dd214b71036a65952209c7fa8aa6
Romain Mormont <romainmormont@hotmail.com>
2020-06-28 16:59:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="app" class="page-container">
+        <layout-navbar></layout-navbar>
+
+        <div class="app-content">
+            <router-view v-if="initialized"></router-view>
+        </div>
+
+        <layout-footer></layout-footer>
+    </div>
+</template>
+
+<script>
+  import LayoutNavbar from './components/layout/LayoutNavbar';
+  import LayoutFooter from './components/layout/LayoutFooter';
+  export default {
+    name: 'ailg',
+    components: {
+      LayoutNavbar,
+      LayoutFooter
+    },
+    computed: {
+      initialized() {
+        return this.$store.state.initialized;
+      }
+    },
+    created() {
+      this.$store.dispatch('initializeStore');
+    }
+  };
+</script>
+
+<style>
+    .page-container {
+        position: relative;
+        min-height: 100vh;
+    }
+
+    .app-content {
+        padding-bottom: 144px;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/style.scss';
+
+import i18n from './i18n';
+
+import VueRouter from 'vue-router';
+
+import moment from 'moment';
+moment.locale(window.navigator.userLanguage || window.navigator.language || 'en');
+moment.tz.setDefault(moment.tz.guess());
+
+import router from './router';
+Vue.use(VueRouter);
+
+import store from './store';
+
+import Buefy from 'buefy';
+Vue.use(Buefy, {defaultIconPack: 'fas'});
+
+// import axios from 'axios';
+// axios.defaults.baseURL = process.env.VUE_APP_API;
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+  i18n,
+  router,
+  store
+}).$mount('#app');
